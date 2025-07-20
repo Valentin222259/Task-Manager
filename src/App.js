14c10f243ad69a5c9d8af81e662bc3d3a6e917df
ardeleanvalentin <ardeleanvalentin490@yahoo.com>
2025-07-20 13:51:42 +0300

    Adaugam task manager

--- src/App.js
+++ src/App.js
@@ -7,42 +7,57 @@ function App() {
       id: "T-1",
       name: "Create a Design System for Enum Workspace.",
       status: "Todo",
-      dueDate: new Date(2022, 5, 23),
+      dueDate: new Date(2021, 1, 24),
     },
     {
       id: "T-2",
       name: "12 Create a Design System for Enum Workspace.",
       status: "In Progress",
-      dueDate: new Date(2022, 7, 24),
+      dueDate: new Date(2021, 1, 24),
     },
     {
       id: "T-3",
       name: "13 Create a Design System for Enum Workspace.",
-      status: "Complete",
-      dueDate: new Date(2022, 6, 13),
+      status: "Completed",
+      dueDate: new Date(2021, 1, 24),
+    },
+    {
+      id: "T-4",
+      name: "13 Create a Design System for Enum Workspace.",
+      status: "Done",
+      dueDate: new Date(2021, 1, 24),
+    },
+    {
+      id: "T-4",
+      name: "13 Create a Design System for Enum Workspace.",
+      status: "In progress",
+      dueDate: new Date(2021, 1, 24),
+    },
+    {
+      id: "T-5",
+      name: "13 Create a Design System for Enum Workspace.",
+      status: "Todo",
+      dueDate: new Date(2021, 1, 24),
+    },
+    {
+      id: "T-6",
+      name: "13 Create a Design System for Enum Workspace.",
+      status: "Completed",
+      dueDate: new Date(2021, 1, 24),
     },
   ];
 
   return (
-    <div className="app-container">
-      <TaskCard
-        id={data[0].id}
-        status={data[0].status}
-        name={data[0].name}
-        dueDate={data[0].dueDate}
-      />
-      <TaskCard
-        id={data[1].id}
-        status={data[1].status}
-        name={data[1].name}
-        dueDate={data[1].dueDate}
-      />
-      <TaskCard
-        id={data[2].id}
-        status={data[2].status}
-        name={data[2].name}
-        dueDate={data[2].dueDate}
-      />
+   <div className="app-container">
+      {data.map((task) => (
+        <TaskCard
+          key={task.id} // cheia unică, necesară în React pentru performanță
+          id={task.id}
+          name={task.name}
+          status={task.status}
+          dueDate={task.dueDate}
+        />
+      ))}
     </div>
   );
 }
--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,3 +1,5 @@
+import Badge from "../badge/Badge";
+import DateContainer from "../date-container/DateContainer";
 import "./TaskCard.css";
 
 function TaskCard(props) {
@@ -5,18 +7,13 @@ function TaskCard(props) {
     <div className="card-wrapper">
       <div className="card-header">
         <p className="task-id">{props.id}</p>
-        <div className="badge">
-          <p>{props.status}</p>
-        </div>
+        <Badge status={props.status} />
       </div>
       <div className="card-content">
         <p>{props.name}</p>
       </div>
       <div className="card-footer">
-        <div className="due-date">
-          <p>Due Date</p>
-          <p>{props.dueDate.toLocaleDateString()}</p>
-        </div>
+        <DateContainer dueDate={props.dueDate} />
       </div>
     </div>
   );
