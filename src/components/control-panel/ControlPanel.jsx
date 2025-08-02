c86358fca8bfd769cce080e37c7e973e8cb9cf5b
ardeleanvalentin <ardeleanvalentin490@yahoo.com>
2025-08-02 11:57:04 +0300

    ???

---
+++ src/components/control-panel/ControlPanel.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import CreateTaskForm from "../forms/CreateTaskForm";
+import Modal from "../modal/Modal";
+import "./ControlPanel.css";
+
+const ControlPanel = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleNewTaskAdd = (task) => {
+    props.addNewTask(task);
+    closeModal();
+  }
+
+  return (
+    <div className="control-panel-container">
+      <div>
+        <h3 className="view-title">Tasks</h3>
+        <p className="subtitle">Your tasks in your space.</p>
+      </div>
+      <button onClick={openModal} className="button-primary">
+        Create Task
+      </button>
+      <Modal onClose={closeModal} isOpen={isOpen}>
+        <h3>Create task</h3>
+        <CreateTaskForm addNewTask = {handleNewTaskAdd} />
+      </Modal>
+    </div>
+  );
+};
+
+export default ControlPanel;
